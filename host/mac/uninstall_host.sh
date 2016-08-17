963868b9360dcc9e82ab3c72fcdcb732b68655d3
sunsagong <sagong.sun@gmail.com>
2016-08-17 10:01:38 +0800

    Added NativeMessaging host installation scripts for Mac/Linux

---
+++ host/mac/uninstall_host.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+set -e
+if [ "$(uname -s)" == "Darwin" ]; then
+  if [ "$(whoami)" == "root" ]; then
+    TARGET_DIR="/Library/Google/Chrome/NativeMessagingHosts"
+  else
+    TARGET_DIR="$HOME/Library/Application Support/Google/Chrome/NativeMessagingHosts"
+  fi
+else
+  if [ "$(whoami)" == "root" ]; then
+    TARGET_DIR="/etc/opt/chrome/native-messaging-hosts"
+  else
+    TARGET_DIR="$HOME/.config/google-chrome/NativeMessagingHosts"
+  fi
+fi
+HOST_NAME=com.google.chrome.example.echo
+rm "$TARGET_DIR/org.factpub.factify.json"
+echo "Native messaging host $HOST_NAME has been uninstalled."
