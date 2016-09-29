1184079c1ff8605601383c5ce11412f49ba78f13
sunsagong <sagong.sun@gmail.com>
2016-09-29 14:37:07 +0800

    Add codes to generate unique token

--- scripts/background.js
+++ scripts/background.js
@@ -33,6 +33,36 @@ function isPdfFile(response, url) {
   }
 }
 
+
+// Codes for generate unique token
+function getRandomToken() {
+    // E.g. 8 * 32 = 256 bits token
+    var randomPool = new Uint8Array(32);
+    crypto.getRandomValues(randomPool);
+    var hex = '';
+    for (var i = 0; i < randomPool.length; ++i) {
+        hex += randomPool[i].toString(16);
+    }
+    // E.g. db18458e2782b2b77e36769c569e263a53885a9944dd0a861e5064eac16f1a
+    return hex;
+}
+
+chrome.storage.sync.get('userid', function(items) {
+    var userid = items.userid;
+    if (userid) {
+        useToken(userid);
+    } else {
+        userid = getRandomToken();
+        chrome.storage.sync.set({userid: userid}, function() {
+            useToken(userid);
+        });
+    }
+    function useToken(userid) {
+        // TODO: Use user id for authentication or whatever you want.
+    }
+});
+
+
 // // 
 // chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
 //     if (tab.url.indexOf('dotinstall') != -1) {
@@ -82,7 +112,7 @@ chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
       chrome.pageAction.show(tabId);
       console.log(url);
       console.log("This page is PDF -- page action is activated.");
-
+      chrome.browserAction.popup({url : "popup.html"}); 
     } else {
       // The page is HTML file
     }
@@ -93,26 +123,10 @@ chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
 
 // Clicked?
 chrome.pageAction.onClicked.addListener(function() {
-    console.log("Page Action is clicked!");
-    console.log(xhr);
-    pdfData = {"responseURL": null, "size":0, "pdf":null};
-
-    ////////////////////////////
-    // bufferarray - scenario //
-    ////////////////////////////
-    byteArray = new Uint8Array(xhr.response);
-    pdfData.responseURL = xhr.responseURL;
-    pdfData.size = byteArray.length;
-    pdfData.pdf = byteArray;
-
-    /////////////////////
-    // output //
-    /////////////////////
-    console.log(pdfData.responseURL)
-    console.log(pdfData.size);
-    console.log(pdfData.pdf);
-    connectNativeApp();
+  runNativeApp();
 });
+
+
 
 function connectNativeApp() {
   console.log("Connecting to NativeApp...");
