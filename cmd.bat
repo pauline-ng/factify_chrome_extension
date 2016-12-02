65d914fc4caa5e7374575789332df87dd3e100ae
Sun Sagong <suns1@gis.a-star.edu.sg>
2016-12-02 14:46:00 +0800

    Improved instruction page for windows by Pauline

---
+++ cmd.bat
@@ -0,0 +1,8 @@
+@echo off
+cd /d %~dp0
+
+echo !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+echo   Your Java is not up-to-date
+echo   Please install the latest java:
+echo   https://www.java.com/download/
+echo !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
